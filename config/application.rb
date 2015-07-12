cc0d5a2959233665cae97438a5fd40e581adea8f
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-07-11 20:24:56 -0400

    Start Splash
    Completed the layout and a lot of the design.
    
    I’ll work on the menu and linking the buttons later.

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,10 @@ module RubyGettingStarted
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    # Add the /app/assets/fonts directory to the asset pipeline
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
+    config.assets.precompile += %w( hover-min.css )
   end
 end
