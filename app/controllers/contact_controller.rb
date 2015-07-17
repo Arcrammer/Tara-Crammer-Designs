d12ba6b68ad3f0104f6e8bcd530f18943bb11f00
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-07-17 11:18:37 -0400

    Finished Contact Page

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -4,5 +4,8 @@ class ContactController < ApplicationController
   def send_message
 #     Uncomment the following line and add credentials to /config/environments/development.rb in order to send messages from Gmail
     OwnerMailer.send_visitor_message(params[:from], params[:with_address], params[:message]).deliver
+    redirect_to :action => 'contacted' # Send the user to the 'Thanks!' page
+  end
+  def contacted
   end
 end
