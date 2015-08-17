d8ba271fad2e227526cabcc3eeb7f5c5adc2850e
Alexander <Alexander2475914@gmail.com>
2015-08-17 15:56:16 -0400

    Configure Assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.precompile += Rails.root.join("vendor", "assets", "images")
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
